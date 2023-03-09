b7aab02fd36c7cbeab47e54150255cf7051d267d
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-03-09 23:05:43 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <h1>Hello Vue</h1>
+  <RouterLink to="/setting">Setting</RouterLink>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 class="text-red-500">Home View</h2>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+
+    return { count };
+  },
+};
+</script>
